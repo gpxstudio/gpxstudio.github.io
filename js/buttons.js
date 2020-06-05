c7a1714809fe1367bc284e19ea8e33e3e5e12091
vcoppe <vianney.coppe@student.uclouvain.be>
2020-06-05 09:00:41 +0200

    correct opentopomap maxzoom

--- js/buttons.js
+++ js/buttons.js
@@ -118,7 +118,7 @@ export default class Buttons {
                     useCache: true,
 	                crossOrigin: true
                 });
-              
+
                 _this.openCycleMap = L.tileLayer('https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey={apikey}', {
                     attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
                     apikey: '67774cfadfeb42d2ac42bc38fda667c0',
@@ -128,7 +128,7 @@ export default class Buttons {
                 });
 
                 _this.openTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-                    maxZoom: 18,
+                    maxZoom: 17,
                     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
                     useCache: true,
 	                crossOrigin: true
