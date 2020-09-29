3c4911c02e722751ac57c5a19ba6a9f2378bcff8
vcoppe <vianney.coppe@student.uclouvain.be>
2020-09-29 11:10:23 +0200

    remove caching for openstreetmap

--- js/buttons.js
+++ js/buttons.js
@@ -142,10 +142,7 @@ export default class Buttons {
                 _this.openStreetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
                     maxZoom: 20,
-                    maxNativeZoom: 19,
-                    useCache: true,
-	                crossOrigin: true,
-                    cacheMaxAge: cacheAge
+                    maxNativeZoom: 19
                 }).addTo(_this.map);
 
                 _this.mapboxStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
