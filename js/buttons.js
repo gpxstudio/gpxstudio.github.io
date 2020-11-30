49c6c490477810a2c0b79a58d51838ec15a8959e
vcoppe <vianney.coppe@student.uclouvain.be>
2020-11-30 12:21:48 +0100

    fix exit edit on escape

--- js/buttons.js
+++ js/buttons.js
@@ -818,10 +818,7 @@ export default class Buttons {
         });
         document.addEventListener("keydown", ({key}) => {
             if (key === "Escape") {
-                if (buttons.window_open) {
-                    buttons.window_open.hide();
-                    return;
-                }
+                if (buttons.window_open) buttons.window_open.hide();
                 if (total.hasFocus) return;
                 var trace = total.traces[total.focusOn];
                 if (trace.isEdited) buttons.edit.click();
