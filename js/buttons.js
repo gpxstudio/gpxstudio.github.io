fb7c600b42e244fb4f0cf87fd6ae8cfb1eeb57bc
vcoppe <vianney.coppe@gmail.com>
2021-01-16 10:34:26 +0100

    use Mapbox Satellite with Streets data

--- js/buttons.js
+++ js/buttons.js
@@ -334,7 +334,7 @@ export default class Buttons {
                     _this.mapboxSatellite = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
                         attribution: '&copy; <a href="https://www.mapbox.com/about/maps/" target="_blank">Mapbox</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>',
                         maxZoom: 20,
-                        id: 'mapbox/satellite-v9',
+                        id: 'mapbox/satellite-streets-v11',
                         tileSize: 512,
                         zoomOffset: -1,
                         accessToken: _this.mapbox_token,
