877aa083a2bc31808a528cf666fc51ee3157201f
shekenz <2657910+shekenz@users.noreply.github.com>
2022-10-19 11:54:22 +0200

    Fixed appending content as string
    https://github.com/mapshakers/leaflet-control-window/issues/19

--- include/controlwindow/L.Control.Window.js
+++ include/controlwindow/L.Control.Window.js
@@ -192,7 +192,13 @@ L.Control.Window = L.Control.extend({
             return this.options.content
         }
         this.options.content = content;
-        this.getContainer().innerHTML = content;
+        
+        if (typeof content === 'string') {
+            this.getContainer().innerHTML = content;
+        } else {
+            this.getContainer().appendChild(content);
+        }
+
         return this;
     },
     prompt : function(promptObject){
